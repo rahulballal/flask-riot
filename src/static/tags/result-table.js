43ab552d075a9e1a3981fdcfcfc2dfd4143ccfad
Rahul Ballal <rahulballal@gmail.com>
2015-12-24 22:29:59 -0800

    completes Riot v1

---
+++ src/static/tags/result-table.js
@@ -0,0 +1,2 @@
+riot.tag2('result-table', '<div class="row"> <div class="columns"> <table class="ui-full-width"> <thead> <tr> <th>Name</th> <th>Brand</th> <th>Calories</th> <th>Total Fat</th> <th>Serving Unit</th> <th>Serving Quantity</th> </tr> </thead> <tbody> <tr> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td> </tr> </tbody> </table> </div> </div>', '', '', function(opts) {
+});
